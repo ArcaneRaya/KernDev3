aee77f5d7ca80241045a069db448bb0a0d4aa53e
Ruben Bergshoeff <rubicalme@gmail.com>
2019-07-04 09:32:38 +0200

    added pause and resume play functionality, added text mesh pro to project

--- WillowAI/Assets/Scripts/Controllers/MainController.cs
+++ WillowAI/Assets/Scripts/Controllers/MainController.cs
@@ -1,11 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainController : MonoBehaviour {
     [SerializeField] private List<BaseController> controllers;
+    [SerializeField] private GameObject pausedUIOverlay;
+
+    private bool isGameRunning;
+
+    public void StartGame() {
+        isGameRunning = true;
+        pausedUIOverlay.SetActive(false);
+    }
+
+    public void PauseGame() {
+        isGameRunning = false;
+        pausedUIOverlay.SetActive(true);
+    }
 
     private void Awake() {
+        isGameRunning = false;
         foreach (BaseController controller in controllers) {
             controller.Initialize(controllers);
         }
@@ -18,8 +33,23 @@ public class MainController : MonoBehaviour {
     }
 
     private void Update() {
+        HandleInput();
+
+        if (isGameRunning == false) { return; }
+
         foreach (BaseController controller in controllers) {
             controller.Tick(Time.deltaTime);
+        }
+    }
+
+    private void HandleInput() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isGameRunning) {
+                PauseGame();
+            }
+            else {
+                StartGame();
+            }
         }
     }
 }
