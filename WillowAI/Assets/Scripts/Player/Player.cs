0865840e9ecadfd3f40ad1d701221c6c45426690
Ruben Bergshoeff <rubicalme@gmail.com>
2019-07-04 12:11:39 +0200

    added monosingleton and made player monosingleton

--- WillowAI/Assets/Scripts/Player/Player.cs
+++ WillowAI/Assets/Scripts/Player/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour, IAgent {
+public class Player : MonoSingleton<Player>, IAgent {
 
     public Vector3 Position {
         get {
